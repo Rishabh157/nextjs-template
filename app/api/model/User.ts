42eb1c0918aa715057866a86842c36fab16b1ca9
mayank joshi <mayankjoshi780@gmail.com>
2024-08-13 15:51:32 +0530

    API changes

---
+++ app/api/model/User.ts
@@ -0,0 +1,16 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+
+interface IUser extends Document {
+  email: string;
+  password: string;
+}
+
+const UserSchema: Schema = new Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const User: Model<IUser> =
+  mongoose.models.User || mongoose.model("User", UserSchema);
+
+export default User;
